50bce4180734c8fb76e13444d782006b0fc331b1
jubaal <jubaal2122daw2@protonmail.com>
2022-03-22 16:43:41 +0100

    creado layout de crear cuenta

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/crearCompte', function(req, res, next) {
+  res.render('crearCompte', { title: 'Express' });
+});
+
+
 module.exports = router;

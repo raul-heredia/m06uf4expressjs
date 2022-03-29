7f9552113a7992680d19bd83c577b841c897b87e
Raúl Heredia <raul.heredia01@outlook.com>
2022-03-29 15:52:22 +0200

    Pàgina d'error i encaminament usuaris

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/iniciarSessio', function (req, res, next) {
+  const usuari = req.body.usuari;
+  const contrasenya = req.body.contrasenya;
+
+  res.send(`Usuari: ${usuari} Contrasenya: ${contrasenya}`);
+});
+
+router.post('/crearCompte', function (req, res, next) {
+  const usuari = req.body.usuari;
+  const contrasenya = req.body.contrasenya;
+  const repContrasenya = req.body.repContrasenya;
+
+  res.send(`Usuari: ${usuari} Contrasenya: ${contrasenya} repContrasenya: ${repContrasenya}`);
+});
+
+
 
 module.exports = router;

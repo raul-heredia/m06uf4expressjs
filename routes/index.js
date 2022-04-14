1603c5b3f1c515493e9d86dfcb0c8471c662bab1
Raúl Heredia <raul.heredia01@outlook.com>
2022-04-14 13:26:22 +0200

    Logica crear partidas 2 y 4 jugadores

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var monk = require('monk');
 var db = monk('localhost:27017/projecteExpress');
 
+let partidas2Jug = [];
+let partidas4Jug = [];
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Iniciar Sessió' });
@@ -22,5 +24,109 @@ router.get('/lobby', function (req, res, next) {
   ]
   res.render('lobby', { title: 'Lobby', username: usuari, arr: arr });
 });
+
+/* LOGICA CREAR UNIRSE PARTIDA 2 JUGADORES */
+
+router.post('/partida2Jug', function (req, res, next) {
+  const codiPartida = req.body.codiPartida;
+  const accio = req.body.accion;
+  const nomJugador = req.body.nomJugador;
+  switch (accio) {
+    case "crear":
+      let isPartida = false;
+      partidas2Jug.forEach(partida => {
+        if (partida.codiPartida == codiPartida) {
+          isPartida = true;
+          res.send("Error, ja existeix la partida");
+        };
+      });
+      if (!isPartida) {
+        let jugadors = [];
+        let jugador = new Jugador(nomJugador)
+        jugadors.push(jugador);
+        let partida = new Partida(codiPartida, jugadors);
+        partidas2Jug.push(partida);
+        console.log(partidas2Jug)
+        console.log(partidas2Jug[0].jugadors)
+      }
+      break;
+    case "unirse":
+      partidas2Jug.forEach(partida => {
+        if (partida.codiPartida == codiPartida) {
+          if (partida.jugadors.length < 2) {
+            if (partida.jugadors[0].nomJugador != nomJugador) {
+              let jugador = new Jugador(nomJugador);
+              partida.jugadors.push(jugador);
+            }
+          } else {
+            console.log("Error", nomJugador, "partida completa")
+          }
+        };
+      });
+      console.log(partidas2Jug)
+      console.log(partidas2Jug[0].jugadors);
+      break;
+  }
+  res.send(`${codiPartida}, ${accio}`);
+});
+router.post('/partida4Jug', function (req, res, next) {
+  const codiPartida = req.body.codiPartida;
+  const accio = req.body.accion;
+  const nomJugador = req.body.nomJugador;
+  switch (accio) {
+    case "crear":
+      let isPartida = false;
+      partidas4Jug.forEach(partida => {
+        if (partida.codiPartida == codiPartida) {
+          isPartida = true;
+          res.send("Error, ja existeix la partida");
+        };
+      });
+      if (!isPartida) {
+        let jugadors = [];
+        let jugador = new Jugador(nomJugador)
+        jugadors.push(jugador);
+        let partida = new Partida(codiPartida, jugadors);
+        partidas4Jug.push(partida);
+        console.log(partidas4Jug)
+        console.log(partidas4Jug[0].jugadors)
+      }
+      break;
+    case "unirse":
+      partidas4Jug.forEach(partida => {
+        if (partida.codiPartida == codiPartida) {
+          if (partida.jugadors.length < 4) {
+            if (partida.jugadors[0].nomJugador != nomJugador) {
+              let jugador = new Jugador(nomJugador);
+              partida.jugadors.push(jugador);
+            }
+          } else {
+            console.log("Error", nomJugador, "partida completa")
+          }
+        };
+      });
+      console.log(partidas4Jug)
+      console.log(partidas4Jug[0].jugadors);
+      break;
+  }
+  res.send(`${codiPartida}, ${accio}`);
+});
+
+class Partida {
+  constructor(codiPartida, jugadors) {
+    this.codiPartida = codiPartida;
+    this.jugadors = jugadors;
+  }
+}
+
+class Jugador {
+  constructor(nomJugador) {
+    this.nomJugador = nomJugador;
+    this.puntuacio = 0;
+  }
+  /* get nomJugador(nombre) {
+    this.nombre = nombre;
+  } */
+}
 
 module.exports = router;
